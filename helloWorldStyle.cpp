42177628b2f4f4736f9be11551c6afb1f989d228
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-09-21 16:55:41 +0000

    Done helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Remy Skelton All right reserved
+// .
+// Created by: Remy Skelton
+// Date: Sep 18 2023
+// This program displays "Hello,World! with style!".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is a lot harder than Python" << std::endl;
+}
